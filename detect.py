3d2a1834f5834b1f72b3a372bbe7af66efb30635
Andrew Magill <amagill@tacc.utexas.edu>
2019-12-06 11:26:50 -0600

    bug fix

--- detect.py
+++ detect.py
@@ -1,3 +1,4 @@
+import os
 from PIL import Image
 from yolo import YOLO
 
@@ -6,12 +7,10 @@ y = YOLO()
 TEST_DIR = "/home/bosworth/work/ILSVRC/Data/CLS-LOC/test"
 test_images = os.listdir(TEST_DIR)
 
-results = []
+f = open("submission_results.csv","w")
 
-f = open("submission_results","w")
-
-for fname in test_images:
-    result = y.detect_image(Image.open(os.join(TEST_DIR, fname)))
+for fname in test_images[:200]:
+    result = y.detect_image(Image.open(os.path.join(TEST_DIR, fname)))
     f.write("%s\n" % " ".join(result[1:]))
 
 f.close()
